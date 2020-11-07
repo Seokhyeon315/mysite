e1201d56c81bc73bf1ec99c5c078ca7e3e4ff943
Seokhyeon315 <shyeon0315@gmail.com>
2020-11-07 20:34:59 +0900

    commit of part 5 django tutorial on 11/7/2020

--- polls/models.py
+++ polls/models.py
@@ -24,3 +24,7 @@ class Choice(models.Model):
         return self.choice_text
 
 
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
--- polls/views.py
+++ polls/views.py
@@ -9,6 +9,9 @@ from .models import Question, Choice
 
 from django.views import generic
 
+from django.utils import timezone
+
+
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -35,16 +38,29 @@ from django.views import generic
 #     question=get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/results.html', {'question': question})
 
+
+#Here we using two generic view: Listview & DetailView
 class IndexView(generic.ListView):
     template_name='polls/index.html'
     context_object_name='latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        # #Return the last five published questions 
+        # return Question.objects.order_by('-pub_date')[:5]
 
+        '''To return the last five published questions (not including those set to be published in the future)'''
+        return Question.objects.filter(pub_date__lte=timezone.now().order_by('-pub_date'))[:5]
+        #it will return a queryset containing Questions whose pub_date is less than or equal to 
+        #that is, earlier than or equal to timezone.now
+
+#After new shortcut function in tests.py, we need to change DetailView class 
 class DetailView(generic.DetailView):
     model=Question
     template_name='polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model=Question
