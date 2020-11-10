1e74b91631c7cad4cfe58d4294f6e1d7e56e75d5
Seokhyeon315 <shyeon0315@gmail.com>
2020-11-10 20:13:50 +0900

    Django tutorial part7 until 'Adding related objects'

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,26 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Question, Choice
+
+# # Register your models here.
+# from .models import Question
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+from django.contrib import admin 
 from .models import Question, Choice
 
-# Register your models here.
-from .models import Question
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields=['pub_date', 'question_text']
 
-admin.site.register(Question)
+# admin.site.register(Question, QuestionAdmin) 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 
